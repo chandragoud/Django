4913e1157d11f4680b8ebcbe8bc24b5e770cdede
chandragoud <chandragoudnaikar23@gmail.com>
2022-06-08 09:32:38 +0530

    added

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
 
+
+from django.urls import path,include
+from . import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('app.urls')),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('',views.index,name='index'),
+    path('buy/<int:pk>/',views.buy,name='buy'),
+    path('pdf/',views.pdf,name='pdf'),
+]
