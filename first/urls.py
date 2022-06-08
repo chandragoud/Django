61cd42901df2203527154067e88776bab0327b1b
chandragoud <chandragoudnaikar23@gmail.com>
2022-06-08 08:40:34 +0530

    added

--- first/urls.py
+++ first/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
     path('home/',views.home_pg,name='home_pg')
 ]
-dsds
+
