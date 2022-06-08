fb316279e2af089a79e03674fc3fd80198b42e75
chandragoud <chandragoudnaikar23@gmail.com>
2022-06-08 10:30:08 +0530

    added

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from . models import seller,buyer,producat
-# Register your models here.
-admin.site.register(seller)
-admin.site.register(buyer)
-admin.site.register(producat)
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,7 @@
 
 
-from django.urls import path,include
-from . import views
+from django.urls import path
+from myapp import views
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('buy/<int:pk>/',views.buy,name='buy'),
-    path('pdf/',views.pdf,name='pdf'),
+    path('response/',views.view1)
 ]
